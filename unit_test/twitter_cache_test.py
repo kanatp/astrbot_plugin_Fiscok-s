79797fb4c9081cca07b14d3f2705d552a25f6cdc
fiscok <16019248+fiscok@user.noreply.gitee.com>
2026-04-05 16:50:28 +0800

    完成推特读取缓存功能（未测试）

--- unit_test/twitter_cache_test.py
+++
@@ -1,6 +0,0 @@
-from ..core.net.twitter_fetch import fetch_twitter_data
-from ..core.api.storage_apis import DataManager
-from astrbot.api import AstrBotConfig
-
-DataManager = DataManager("test_data_path", AstrBotConfig())
-fetch_twitter_data("aimi_sound", DataManager)
